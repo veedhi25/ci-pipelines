21fe1c38ea7084033aa42f3103e2fef60d664420
veedhi25 <142788912+veedhi25@users.noreply.github.com>
2026-05-07 00:46:53 +0530

    Update app.js

--- service1/app.js
+++ service1/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Service1 API is working!");
+  res.send("Service1 API is working! keep going ");
 });
 
 app.listen(80, () => console.log("Service1 running on port 80"));
